203e79531c2b24b3b5407e082b8f7706cf048829
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2024-10-21 12:48:30 -0500

    feat: :sparkles: Add new features
    Add connection string postgres

--- src/main/java/com/appsecurity/security_app/config/security/HttpSecurityConfig.java
+++ src/main/java/com/appsecurity/security_app/config/security/HttpSecurityConfig.java
@@ -3,22 +3,22 @@ package com.appsecurity.security_app.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
 
 import com.appsecurity.security_app.config.security.filter.JwtAuthenticationFilter;
-import com.appsecurity.security_app.infrastructure.utils.enums.RoleEnum;
+
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/appsecurity/security_app/infrastructure/repositories/role/RoleImpl.java
+++ src/main/java/com/appsecurity/security_app/infrastructure/repositories/role/RoleImpl.java
@@ -3,10 +3,12 @@ package com.appsecurity.security_app.infrastructure.repositories.role;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import com.appsecurity.security_app.application.services.IRoleService;
 import com.appsecurity.security_app.domain.entities.security.Role;
 
+@Service
 public class RoleImpl implements IRoleService {
     @Autowired
     private RoleRepository roleRepository;
