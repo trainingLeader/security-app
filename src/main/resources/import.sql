c6b1966f11e29f69d240a6e9f0ec5c3777e8ef96
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2024-10-21 07:57:00 -0500

    feat: :sparkles: Add new feature
    Add import file sql sentences

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,61 @@
+-- CREACIÓN DE MODULOS
+INSERT INTO module (name, base_path) VALUES ('PRODUCT', '/products');
+INSERT INTO module (name, base_path) VALUES ('CATEGORY', '/categories');
+INSERT INTO module (name, base_path) VALUES ('CUSTOMER', '/customers');
+INSERT INTO module (name, base_path) VALUES ('AUTH', '/auth');
+
+-- CREACIÓN DE OPERACIONES
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_ALL_PRODUCTS','', 'GET', false, 1);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_ONE_PRODUCT','/[0-9]*', 'GET', false, 1);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('CREATE_ONE_PRODUCT','', 'POST', false, 1);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('UPDATE_ONE_PRODUCT','/[0-9]*', 'PUT', false, 1);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('DISABLE_ONE_PRODUCT','/[0-9]*/disabled', 'PUT', false, 1);
+
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_ALL_CATEGORIES','', 'GET', false, 2);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_ONE_CATEGORY','/[0-9]*', 'GET', false, 2);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('CREATE_ONE_CATEGORY','', 'POST', false, 2);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('UPDATE_ONE_CATEGORY','/[0-9]*', 'PUT', false, 2);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('DISABLE_ONE_CATEGORY','/[0-9]*/disabled', 'PUT', false, 2);
+
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_ALL_CUSTOMERS','', 'GET', false, 3);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('REGISTER_ONE','', 'POST', true, 3);
+
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('AUTHENTICATE','/authenticate', 'POST', true, 4);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('VALIDATE-TOKEN','/validate-token', 'GET', true, 4);
+INSERT INTO operation (name, path, http_method, permit_all, module_id) VALUES ('READ_MY_PROFILE','/profile','GET', false, 4);
+
+-- CREACIÓN DE ROLES
+INSERT INTO role (name) VALUES ('CUSTOMER');
+INSERT INTO role (name) VALUES ('ASSISTANT_ADMINISTRATOR');
+INSERT INTO role (name) VALUES ('ADMINISTRATOR');
+
+-- CREACIÓN DE PERMISOS
+INSERT INTO granted_permission (role_id, operation_id) VALUES (1, 15);
+
+INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 1);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 2);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 4);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 6);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 7);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 9);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (2, 15);
+
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 1);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 2);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 3);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 4);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 5);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 6);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 7);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 8);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 9);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 10);
+INSERT INTO granted_permission (role_id, operation_id) VALUES (3, 15);
+-- CREACIÓN DE USUARIOS
+INSERT INTO "user" (username, name, password, role_id) VALUES ('jpardo', 'Johlver Pardo', '$2a$10$ywh1O2EwghHmFIMGeHgsx.9lMw5IXpg4jafeFS.Oi6nFv0181gHli', 1);
+INSERT INTO "user" (username, name, password, role_id) VALUES ('jmpardo', 'Jose Manuel', '$2a$10$V29z7/qC9wpHfzRMxGOHye5RMAxCid2/MzJalk0dsiA3zZ9CJfub.', 2);
+INSERT INTO "user" (username, name, password, role_id) VALUES ('jjplata', 'Juan Jose', '$2a$10$TMbMuEZ8utU5iq8MOoxpmOc6QWQuYuwgx1xJF8lSMNkKP3hIrwYFG', 3);
+
 -- CREACIÓN DE CATEGORIAS
 INSERT INTO category (name, status) VALUES ('Computacion y Tecnologia', 'ENABLED');
 INSERT INTO category (name, status) VALUES ('Ropa', 'ENABLED');
